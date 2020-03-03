f4745f478b215c65754df0077d1574e4a941e3b6
liujjpp <liujjpp@gmail.com>
2020-03-02 22:22:52 -0600

    Add some options

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from transforms import to_thai_cuisine
 from transforms import vegetarian_transform
 
 def output_recipe(recipe_data):
-    print('\nIngredients')
+    print('\n[INGREDIENTS]')
     for ingredient in recipe_data['ingredients']:
         s = ' -'
         if ingredient['quantity']:
@@ -26,30 +26,52 @@ def output_recipe(recipe_data):
             s = s + ', ' + ingredient['preparation']
         print(s)
 
-    print('\nDirections')
+    print('\n[STEPS]')
     count = 1
     for direction in recipe_data['directions']:
-        s = str(count) + ' ' + direction['text']
+        s = 'Step ' + str(count) + ': ' + direction['text']
         print(s)
         count += 1
 
-    print('\nNutrition')
+    print('\n[NUTRITION]')
     for n in recipe_data['nutrition']:
-        s = ' - ' + n['name'] + ': ' + n['amount'] + n['unit'] + '          ' + 'Daily Value: '
+        tab = '\t\t\t'
+        if 'Carbohydrates' in n['name']:
+            tab = '\t\t'
+        elif 'Sugars' in n['name'] or 'Iron' in n['name'] or 'Thiamin' in n['name'] or 'Niacin' in n['name']:
+            tab = '\t\t\t\t'
+        s = ' - ' + n['name'] + ': ' + n['amount'] + n['unit'] + tab + 'Daily Value: '
         if n['daily_value']:
             s = s + n['daily_value']
         else:
             s += 'None'
         print(s)
 
+def get_name(recipe_url):
+    if '/?' in recipe_url:
+        i = recipe_url.index('/?')
+        recipe_url = recipe_url[:i + 1]
+    if recipe_url[-1] == '/':
+        recipe_url = recipe_url[:-1]
+    i = recipe_url.index('recipe/')
+    recipe_url = recipe_url[i + 7:]
+    i = recipe_url.index('/')
+    name = recipe_url[i + 1:]
+    name = ' '.join(name.split('-')).upper()
+    return name
+
 def main():
     url = input('Input URL: ')
+    name = get_name(url)
     rf = fetcher.RecipeFetcher()
     recipe_data = parser.parse_recipe(rf.scrape_recipe(url))
-    options = '\nOptions:\n1 To vegetarian\n2 From vegetarian\n3 To healthy\n4 From healthy\n5 Double the amount\n6 Cut it by half\n7 To Thai style\n8 To Japanese style\n9 Exit\nInput your choice (a number): '
+    options = '\nOptions:\n1 Make it vegetarian\n2 Make it un-vegetarian\n3 Make it healthier\n4 Make it less healthy\n5 Double the amount\n6 Cut it by half\n7 To Thai style\n8 To Japanese style\n9 Input a new URL\n10 Print internal representation\n11 Exit\nInput your choice (a number): '
+    print('====================')
+    print(name)
     output_recipe(recipe_data)
+    print('====================')
     choice = input(options)
-    while choice != '9':
+    while choice != '11':
         if choice == '1':
             recipe_data = vegetarian_transform.to_vegetarian(recipe_data)
         elif choice == '2':
@@ -66,57 +88,20 @@ def main():
             recipe_data = to_thai_cuisine.transform_to_thai(recipe_data)
         elif choice == '8':
             pass
+        elif choice == '9':
+            url = input('Input URL: ')
+            name = get_name(url)
+            recipe_data = parser.parse_recipe(rf.scrape_recipe(url))
+        elif choice == '10':
+            print(recipe_data)
+            choice = input(options)
+            continue
+        print('====================')
+        print(name)
         output_recipe(recipe_data)
+        print('====================')
         choice = input(options)
 
 
-if __name__ == '__main__':    
-    example = {
-        'ingredients': [
-            {'name': 'whole wheat lasagna noodles', 'quantity': 12, 'measurement': None, 'descriptor': None, 'preparation': None},
-            {'name': 'ground beef', 'quantity': 1, 'measurement': 'pound', 'descriptor': 'lean', 'preparation': None},
-            {'name': 'garlic', 'quantity': 2, 'measurement': 'cloves', 'descriptor': None, 'preparation': 'chopped'},
-            {'name': 'garlic powder', 'quantity': 0.5, 'measurement': 'teaspoon', 'descriptor': None, 'preparation': None},
-            {'name': 'oregano', 'quantity': 1, 'measurement': 'teaspoon or to taste', 'descriptor': 'dried', 'preparation': None},
-            {'name': 'salt and ground black pepper', 'quantity': None, 'measurement': 'to taste', 'descriptor': None, 'preparation': None},
-            {'name': 'cottage cheese', 'quantity': 1, 'measurement': '(16 ounce) package', 'descriptor': None, 'preparation': None},
-            {'name': 'eggs', 'quantity': 2, 'measurement': None, 'descriptor': None, 'preparation': None},
-            {'name': 'Parmesan cheese', 'quantity': 0.5, 'measurement': 'cup', 'descriptor': None, 'preparation': 'shredded'},
-            {'name': 'tomato-basil pasta sauce', 'quantity': 1.5, 'measurement': '(25 ounce) jars', 'descriptor': None, 'preparation': None},
-            {'name': 'mozzarella cheese', 'quantity': 2, 'measurement': 'cups', 'descriptor': None, 'preparation': 'shredded'}],
-        'directions': [
-            {'text': 'Preheat oven to 350 degrees F (175 degrees C).', 
-             'ingredients': [], 'tools': ['oven'], 'methods': [], 'times': []},
-            {'text': 'Fill a large pot with lightly salted water and bring to a rolling boil over high heat. Once the water is boiling, add the lasagna noodles a few at a time, and return to a boil. Cook the pasta uncovered, stirring occasionally, until the pasta has cooked through, but is still firm to the bite, about 10 minutes. Remove the noodles to a plate.', 
-             'ingredients': ['lasagna noodles', 'pasta', 'noodles'], 'tools': ['pot', 'plate'], 'methods': ['stirring'], 'times': ['10 minutes']},
-            {'text': 'Place the ground beef into a skillet over medium heat, add the garlic, garlic powder, oregano, salt, and black pepper to the skillet. Cook the meat, chopping it into small chunks as it cooks, until no longer pink, about 10 minutes. Drain excess grease.', 
-             'ingredients': ['ground beef', 'garlic', 'garlic powder', 'oregano', 'salt', 'black pepper', 'meat'], 'tools': ['skillet'], 'methods': ['chopping'], 'times': ['10 minutes']},
-            {'text': 'In a bowl, mix the cottage cheese, eggs, and Parmesan cheese until thoroughly combined.', 
-             'ingredients': ['cottage cheese', 'eggs', 'Parmesan cheese'], 'tools': ['bowl'], 'methods': ['mix'], 'times': []},
-            {'text': 'Place 4 noodles side by side into the bottom of a 9x13-inch frying pan; top with a layer of the tomato-basil sauce, a layer of ground beef mixture, and a layer of the cottage cheese mixture. Repeat layers twice more, ending with a layer of sauce; sprinkle top with the mozzarella cheese. Cover the dish with aluminum foil.', 
-             'ingredients': ['noodles', 'tomato-basil sauce', 'ground beef', 'cottage cheese', 'sauce', 'mozzarella cheese'], 'tools': ['frying pan', 'aluminum foil'], 'methods': ['sprinkle'], 'times': []},
-            {'text': 'Bake in the preheated oven until the casserole is bubbling and the cheese has melted, about 30 minutes. Remove foil and bake until cheese has begun to brown, about 10 more minutes. Allow to stand at least 10 minutes before serving.', 
-             'ingredients': ['cheese'], 'tools': ['oven', 'foil'], 'methods': ['Bake', 'bake'], 'times': ['30 minutes', '10 more minutes', '10 minutes']}],
-        'nutrition': [
-            {'name': 'Total Fat', 'amount': '19.3', 'unit': 'g', 'daily_value': '30 %'},
-            {'name': 'Saturated Fat', 'amount': '9.0', 'unit': 'g', 'daily_value': None},
-            {'name': 'Cholesterol', 'amount': '115', 'unit': 'mg', 'daily_value': '38 %'},
-            {'name': 'Sodium', 'amount': '999', 'unit': 'mg', 'daily_value': '40 %'},
-            {'name': 'Potassium', 'amount': '717', 'unit': 'mg', 'daily_value': '20 %'},
-            {'name': 'Total Carbohydrates', 'amount': '47.1', 'unit': 'g', 'daily_value': '15 %'},
-            {'name': 'Dietary Fiber', 'amount': '6.3', 'unit': 'g', 'daily_value': '25 %'},
-            {'name': 'Protein', 'amount': '35.6', 'unit': 'g', 'daily_value': '71 %'},
-            {'name': 'Sugars', 'amount': '12', 'unit': 'g', 'daily_value': None},
-            {'name': 'Vitamin A', 'amount': '855', 'unit': 'IU', 'daily_value': None},
-            {'name': 'Vitamin C', 'amount': '2', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Calcium', 'amount': '361', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Iron', 'amount': '4', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Thiamin', 'amount': '0', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Niacin', 'amount': '11', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Vitamin B6', 'amount': '0', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Magnesium', 'amount': '74', 'unit': 'mg', 'daily_value': None},
-            {'name': 'Folate', 'amount': '41', 'unit': 'mcg', 'daily_value': None}]
-    }
-
-    # output_recipe(example)
+if __name__ == '__main__':
     main()
